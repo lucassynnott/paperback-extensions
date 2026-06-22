131d48b5239dec053c7293605cf2c1fc2c9fdb8a
Lucas Synnott <stopmoclay@gmail.com>
2026-06-22 02:18:06 +0100

    chore: bump ReadComicsOnline RU cache version

--- src/ReadComicsOnlineRu/pbconfig.ts
+++ src/ReadComicsOnlineRu/pbconfig.ts
@@ -5,7 +5,7 @@ import { ContentRating, SourceIntents, type ExtensionInfo } from "@paperback/typ
 export default {
   name: "Read Comics Online RU",
   description: "Paperback extension for readcomicsonline.ru.",
-  version: "1.0.5",
+  version: "1.0.6",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.EVERYONE,
